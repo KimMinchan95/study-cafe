83c09db8f8689d53af6a6412e27888077bf65f27
Kim MinChan <hialscks@gmail.com>
2026-02-05 00:29:32 +0900

    refactor(layout): 메인 컨테이너 스타일 및 구조 변경

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <>
+        <main className="container">
             <div className="text-black dark:text-white">hi</div>
-        </>
+        </main>
     );
 }
